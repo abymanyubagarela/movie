dfc2ae6e21cd4b053a9f8d5e676611094f37506d
abymanyubagarela <abymanyubagarela@gmail.com>
2020-09-11 16:12:52 +0700

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl } from 'react-bootstrap';
+import './Header.scss';
+import logo from '../../assets/logo-min.png';
 
 class Header extends Component {
   render() {
     return (
-      <Navbar bg='light' expand='lg'>
-        <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+      <Navbar expand='lg'>
+        <Navbar.Brand href='#home'>
+          <img className='image_header' src={logo} alt='logo' />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='mr-auto'>
-            <Nav.Link href='#home'>Home</Nav.Link>
-            <Nav.Link href='#link'>Link</Nav.Link>
+            <Nav.Link href='#home'>Live</Nav.Link>
+            <Nav.Link href='#link'>Movie</Nav.Link>
+            <Nav.Link href='#link'>TV Show</Nav.Link>
+            <Nav.Link href='#link'>Sport</Nav.Link>
           </Nav>
+          <Form inline>
+            <FormControl
+              type='text'
+              placeholder='Search'
+              className='mr-sm-2 search-box'
+            />
+          </Form>
         </Navbar.Collapse>
       </Navbar>
     );
