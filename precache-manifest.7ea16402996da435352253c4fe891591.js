74595523c043208a069ec29707ebedb853f01c06
abymanyubagarela <abymanyubagarela@gmail.com>
2020-09-12 17:24:44 +0700

    Updates

---
+++ precache-manifest.7ea16402996da435352253c4fe891591.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4c865ae1e582fda56e276f0e129531",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "ee0c2aac08fb591e7d5f",
+    "url": "/movie/static/css/main.585d849c.chunk.css"
+  },
+  {
+    "revision": "2dfaeafbb5ff6c5ec1ef",
+    "url": "/movie/static/js/2.3367dad3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movie/static/js/2.3367dad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0c2aac08fb591e7d5f",
+    "url": "/movie/static/js/main.4cc74c28.chunk.js"
+  },
+  {
+    "revision": "2a01980b6f3e5e3b4010",
+    "url": "/movie/static/js/runtime-main.2b4e58bf.js"
+  },
+  {
+    "revision": "28fce2abb87556c6622bc642b26438f5",
+    "url": "/movie/static/media/search.28fce2ab.svg"
+  }
+]);
